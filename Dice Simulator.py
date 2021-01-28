53273ef19c6a9aab07e5c2b70620b6833999513e
Augusto Costa <78105114+Augusto-Costa@users.noreply.github.com>
2021-01-28 20:12:17 -0300

    With interface

--- Dice Simulator.py
+++ Dice Simulator.py
@@ -1,21 +1,35 @@
 #Simulador de dados
 #Simular o uso de um dado gerando um valor de 1 até 6
 import random
-
+import PySimpleGUI as sg
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = "Voce gostaria de gerar um novo valor para o dado?"
+    #Layout
+        self.layout = [
+            [sg.Text("Jogar o Dado? ")],
+            [sg.Button("Sim") , sg.Button("Não")]
+        ]
     
-    def Iniciar(self):
-        resposta = input(self.mensagem)
-        try:
-            if(resposta == "sim" or resposta == "s"):
-                self.GerarValordoDado()
-            elif(resposta == "não" or resposta == "n"):
-                print("Ok, agradecemos sua participação ")
 
+    def Iniciar(self):
+
+        #Janela
+        self.janela = sg.Window("Simulador de Dado " , layout = self.layout)
+
+        #Ler os valores da tela
+
+        self.eventos , self.valores = self.janela.Read()
+        
+        #Fazer alguma coisa com esses valores
+        
+        try:
+            if self.eventos == "Sim" or self.eventos =="s":
+                self.GerarValordoDado()
+            elif self.eventos == "Não" or self.eventos == "n":
+                print("Ok, agradecemos sua participação ")
             else:
                 print("Favor digitar sim ou não")
         except:
